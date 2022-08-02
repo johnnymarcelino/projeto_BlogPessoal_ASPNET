5140b3ffcea6e4aba76a5b9ca4fe49aaf891b061
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-08-02 11:45:07 -0300

    @johnnymarcelino [task:commit inicial] {02/08/2022 11:45}

---
+++ BlogTeste/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlogTeste {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
